2879311da17a93e9c7df3869d0b5233ed72a7266
KIRILL OTCHIK <67601079+otchik-k@users.noreply.github.com>
2025-08-15 15:26:13 +0300

    Создан первый вариант интерфейса

--- UserCreator/Form1.Designer.cs
+++ UserCreator/Form1.Designer.cs
@@ -28,12 +28,166 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            textBoxGroupNumber = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBoxOU = new TextBox();
+            textBoxUserCount = new TextBox();
+            textBoxStartNumber = new TextBox();
+            buttonCreate = new Button();
+            label5 = new Label();
+            textBoxPass = new TextBox();
+            textBoxNetDiskAdress = new TextBox();
+            label6 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 44);
+            label1.Name = "label1";
+            label1.Size = new Size(89, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Номер группы";
+            // 
+            // textBoxGroupNumber
+            // 
+            textBoxGroupNumber.Location = new Point(290, 41);
+            textBoxGroupNumber.Name = "textBoxGroupNumber";
+            textBoxGroupNumber.Size = new Size(498, 23);
+            textBoxGroupNumber.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 73);
+            label2.Name = "label2";
+            label2.Size = new Size(212, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Количество учетных записей группы";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 102);
+            label3.Name = "label3";
+            label3.Size = new Size(195, 15);
+            label3.TabIndex = 3;
+            label3.Text = "Стартовый номер учетной записи";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 15);
+            label4.Name = "label4";
+            label4.Size = new Size(272, 15);
+            label4.TabIndex = 6;
+            label4.Text = "OU в которую будут помещены учетные записи";
+            // 
+            // textBoxOU
+            // 
+            textBoxOU.Location = new Point(290, 12);
+            textBoxOU.Name = "textBoxOU";
+            textBoxOU.Size = new Size(498, 23);
+            textBoxOU.TabIndex = 8;
+            // 
+            // textBoxUserCount
+            // 
+            textBoxUserCount.Location = new Point(290, 70);
+            textBoxUserCount.Name = "textBoxUserCount";
+            textBoxUserCount.Size = new Size(498, 23);
+            textBoxUserCount.TabIndex = 9;
+            // 
+            // textBoxStartNumber
+            // 
+            textBoxStartNumber.Location = new Point(290, 99);
+            textBoxStartNumber.Name = "textBoxStartNumber";
+            textBoxStartNumber.Size = new Size(498, 23);
+            textBoxStartNumber.TabIndex = 10;
+            // 
+            // buttonCreate
+            // 
+            buttonCreate.Font = new Font("Segoe UI", 72F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            buttonCreate.Location = new Point(12, 318);
+            buttonCreate.Name = "buttonCreate";
+            buttonCreate.Size = new Size(776, 170);
+            buttonCreate.TabIndex = 11;
+            buttonCreate.Text = "СОЗДАТЬ!";
+            buttonCreate.UseVisualStyleBackColor = true;
+            buttonCreate.Click += buttonCreate_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(12, 131);
+            label5.Name = "label5";
+            label5.Size = new Size(110, 15);
+            label5.TabIndex = 12;
+            label5.Text = "Стартовый пароль";
+            // 
+            // textBoxPass
+            // 
+            textBoxPass.Location = new Point(290, 128);
+            textBoxPass.Name = "textBoxPass";
+            textBoxPass.Size = new Size(498, 23);
+            textBoxPass.TabIndex = 13;
+            // 
+            // textBoxNetDiskAdress
+            // 
+            textBoxNetDiskAdress.Location = new Point(290, 157);
+            textBoxNetDiskAdress.Name = "textBoxNetDiskAdress";
+            textBoxNetDiskAdress.Size = new Size(498, 23);
+            textBoxNetDiskAdress.TabIndex = 14;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(12, 160);
+            label6.Name = "label6";
+            label6.Size = new Size(174, 15);
+            label6.TabIndex = 15;
+            label6.Text = "Расположение сетевого диска";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 500);
+            Controls.Add(label6);
+            Controls.Add(textBoxNetDiskAdress);
+            Controls.Add(textBoxPass);
+            Controls.Add(label5);
+            Controls.Add(buttonCreate);
+            Controls.Add(textBoxStartNumber);
+            Controls.Add(textBoxUserCount);
+            Controls.Add(textBoxOU);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBoxGroupNumber);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox textBoxGroupNumber;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBoxOU;
+        private TextBox textBoxUserCount;
+        private TextBox textBoxStartNumber;
+        private Button buttonCreate;
+        private Label label5;
+        private TextBox textBoxPass;
+        private TextBox textBoxNetDiskAdress;
+        private Label label6;
     }
 }
--- UserCreator/Form1.cs
+++ UserCreator/Form1.cs
@@ -6,5 +6,23 @@ namespace UserCreator
         {
             InitializeComponent();
         }
+
+        string ouPath = "";
+        string[] newUser = new string[] {
+            /*1 Выводимое имя */   "newUserDisplayName",
+            /*2 логин */           "newUserLogin",
+            /*3 ппароль */         "newUserPass",
+            /*4 Путь к общему диску */ "HomeDirectory"
+        };
+
+        public static void AdUserCreator(string ou, string[] newUserData)
+        {
+
+        }
+
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
